0aaae852f5cd5ccd02fa4de18038e5adc6257fe3
2Smean <136636643+2Smean@users.noreply.github.com>
2024-08-21 20:38:20 +0900

    feat : toString() 20240821

---
+++ src/lang/object/tostring/ToStringMain2.java
@@ -0,0 +1,29 @@
+package lang.object.tostring;
+
+public class ToStringMain2 {
+
+    public static void main(String[] args) {
+        Car car = new Car("Model Y");
+        Dog dog1 = new Dog("진돗개", 2);
+        Dog dog2 = new Dog("삽살개", 3);
+
+        System.out.println("1. 단순 toString 호출");
+        System.out.println(car.toString());
+        System.out.println(dog1.toString());
+        System.out.println(dog2.toString());
+
+        System.out.println("2. println 내부에서 toString 호출");
+        System.out.println(car);
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        System.out.println("3. Object 다형성 활용");
+        ObjectPrinter.print(car);
+        ObjectPrinter.print(dog1);
+        ObjectPrinter.print(dog2);
+
+        //overriding 되지 않은 참조값을 보고 싶을 때
+        String refValue = Integer.toHexString(System.identityHashCode(dog1));
+        System.out.println("refValue = " + refValue);
+    }
+}
